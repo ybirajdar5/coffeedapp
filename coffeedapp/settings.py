efb12ec40e9643105c36f0d982a22fb56108a975
Yash Birajdar <ybirajdar5@gmail.com>
2015-09-22 20:45:49 +0530

    Changes maade to imp files

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -82,6 +82,16 @@ DATABASES = {
     }
 }
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
--- coffeedapp/wsgi.py
+++ coffeedapp/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
+import os 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeedapp.settings")
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
