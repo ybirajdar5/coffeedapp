bef6fd74862dae43a99c3d54c49b42d818aab2f8
Yash Birajdar <ybirajdar5@gmail.com>
2015-09-28 10:09:12 +0530

    Added file

--- coffeedapp/wsgi.py
+++ coffeedapp/wsgi.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeedapp.settings")
-
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
 application = Cling(get_wsgi_application())
